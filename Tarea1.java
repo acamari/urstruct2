1c0eb9c5c1f8261cc3a5e8e889727f8967e52e52
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2013-02-14 00:44:39 -0600

    adds delete support, sync regress

--- Tarea1.java
+++ Tarea1.java
@@ -1,10 +1,33 @@
-import java.io.*; // para leer de la entrada estandar
+// Ejecuta comandos desde la entrada estándar (consola), actualmente reconoce
+// dos tipos de comandos, este programa siempre imprime el estado final de la
+// lista ordenada
+//
+// Insertar en la lista ordenada, para insertar, escríbase cualquier dato que
+// no esté precedido por un guión (-) y presiónese enter, ejemplo:
+//	$ java Tarea1 
+//	mundo
+//	hola
+//	^D
+//	hola
+//	mundo
+//	$
+//
+// Remover de la lista ordenada, escríbase cualquier dato precedido de un
+// guión, y será removido de la lista ordenada:
+//	$ java Tarea1 
+//	c
+//	a
+//	b
+//	-a
+//	^D
+//	b
+//	c
+//	$
+import java.io.*; 
 
 
 public class Tarea1 {
 	public static void main(String[] args) {
-		// lee línea por línea y guarda una lista usando cada línea
-		// como el dato de un nodo
 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		String linea = null;
 		ListaDoble l = new ListaDoble();
@@ -16,7 +39,15 @@ public class Tarea1 {
 			}
 			if (linea == null) break; // fin de lectura
 			linea = linea.replace("\n", ""); // borra saltos de línea
-			if (l.inserta(new Nodo(linea)) != true) {
+
+			// borrar
+			if (linea.matches("^-.*$")) {
+				linea = linea.replaceFirst("-", "");
+				if (l.rm(new Nodo(linea)) <= 0) {
+					System.err.println("no se encontro: " + linea);
+				}
+			// insertar
+			} else if (l.inserta(new Nodo(linea)) != true) {
 				System.err.println("error: insertando " + linea);
 			}
 
@@ -57,7 +88,7 @@ class ListaDoble {
 			// actual es lógicamente mayor al nodo a insertar
 			// entonces, inserta antes de él
 			if (p.dato.compareTo(n.dato) > 0) {
-				//System.out.println(n.dato + " < " + p.dato);
+				//System.err.println(n.dato + " < " + p.dato);
 				// no es el primer elemento
 				if (p.prev != null) p.prev.sig = n;
 				n.prev = p.prev;
@@ -68,7 +99,7 @@ class ListaDoble {
 
 			// actual es el último nodo, inserta después de él
 			} else if (p.sig == null) {
-				//System.out.println("last: " + n.dato + " < " + p.dato);
+				//System.err.println("last: " + n.dato + " < " + p.dato);
 				p.sig = n;
 				n.prev = p;
 				n.sig = null;
@@ -77,8 +108,23 @@ class ListaDoble {
 		}
 		return false;
 	}
-	// remueve nodo de la lista...
-	public void rm(Nodo n) { 
+	// remueve de la lista todos los nodos cuyo dato sea lógicamente igual a
+	// n.dato, regresa número de elementos eliminados
+	public int rm(Nodo n) { 
+		int el = 0; // eliminados
+		//System.err.println("borrando: " + n.dato);
+		for (Nodo p = pri; p != null; p = p.sig) {
+			if (!p.dato.equals(n.dato)) continue;
+			if (p.prev == null && p.sig == null) { // único elem
+				pri = null;
+			} else if (p.prev == null) { // primer elem
+				p.sig.prev = null;
+				pri = p.sig;
+				p.sig = null;
+			}
+			++el;
+		}
+		return el;
 	}
 	// imprime lista
 	public void imprime() {
