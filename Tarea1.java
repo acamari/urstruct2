ebb0f010704137938e213c64cfe9f189cc3a0571
Abel Abraham Camarillo Ojeda <acamari@verlet.org>
2013-02-14 03:34:25 -0600

    adds support for modification of nodes

--- Tarea1.java
+++ Tarea1.java
@@ -1,6 +1,8 @@
 // Ejecuta comandos desde la entrada estándar (consola), actualmente reconoce
-// dos tipos de comandos, este programa siempre imprime el estado final de la
-// lista ordenada
+// tres tipos de comandos, además este programa siempre imprime el estado final
+// de la lista ordenada:
+//
+// Comandos:
 //
 // Insertar en la lista ordenada, para insertar, escríbase cualquier dato que
 // no esté precedido por un guión (-) y presiónese enter, ejemplo:
@@ -23,8 +25,23 @@
 //	b
 //	c
 //	$
+//
+//
+// Modificar un elemento de la lista ordenada, esto se hace precediendo el dato
+// a modificar con una tilde (~), y escribiendo el dato a reemplazar precedido
+// por otra ~:
+//	$ java Tarea1 
+//	c
+//	a
+//	b
+//	~a~z
+//	^D
+//	b
+//	c
+//	z
+//	$
 import java.io.*; 
-
+import java.util.regex.*;
 
 public class Tarea1 {
 	public static void main(String[] args) {
@@ -43,14 +60,27 @@ public class Tarea1 {
 			// borrar
 			if (linea.matches("^-.*$")) {
 				linea = linea.replaceFirst("-", "");
-				if (l.rm(new Nodo(linea)) <= 0) {
+				if (l.remueve(new Nodo(linea)) <= 0) {
 					System.err.println("no se encontro: " + linea);
 				}
+			// modificar
+			} else if (linea.matches("^~.*$")) {
+				Pattern p = Pattern.compile("^~([^~]*)~(.*)$");
+				Matcher	m = p.matcher(linea);
+				String viejo, nuevo;
+				if (!m.matches()) {
+					System.err.println("comando invalido: " + linea);
+				}
+				viejo = m.group(1);
+				nuevo = m.group(2);
+				if (l.mod(new Nodo(viejo), new Nodo(nuevo)) <= 0) {
+					System.err.println("error en comando: " + linea);
+				}
+			
 			// insertar
 			} else if (l.inserta(new Nodo(linea)) != true) {
 				System.err.println("error: insertando " + linea);
 			}
-
 		}
 		l.imprime();
 	}
@@ -61,10 +91,15 @@ class Nodo {
 	public String		dato	= null;
 	private Nodo		prev	= null;
 	private Nodo		sig	= null;
+	
+	public Nodo(Nodo arg) { //duplicador
+		this.dato = arg.dato;
+		this.prev = arg.prev;
+		this.sig = arg.sig;
+	}
 
 	public Nodo(String arg) {
 		this.dato = arg;
-		return;
 	}
 	public Nodo obtenprev() {
 		return this.prev;
@@ -112,7 +147,8 @@ class ListaDoble {
 
 			// actual es el último nodo, inserta después de él
 			} else if (p.obtensig() == null) {
-				//System.err.println("last: " + n.dato + " < " + p.dato);
+				//System.err.println("last: p: " + p + " dato: " + p.dato + " prev: " + p.obtenprev() + " sig: " + p.obtensig());
+				//System.err.println("last: n: " + n + " dato: " + n.dato + " prev: " + n.obtenprev() + " sig: " + n.obtensig());
 				p.modsig(n);
 				n.modprev(p);
 				n.modsig(null);
@@ -123,26 +159,58 @@ class ListaDoble {
 	}
 	// remueve de la lista todos los nodos cuyo dato sea lógicamente igual a
 	// n.dato, regresa número de elementos eliminados
-	public int rm(Nodo n) { 
+	public int remueve(Nodo n) { 
 		int el = 0; // eliminados
-		//System.err.println("borrando: " + n.dato);
-		for (Nodo p = pri; p != null; p = p.obtensig()) {
-			if (!p.dato.equals(n.dato)) continue;
+		for (Nodo p = pri; p != null;) {
+			if (!p.dato.equals(n.dato)) {
+				p = p.obtensig();
+				continue;
+			}
 			if (p.obtenprev() == null && p.obtensig() == null) { // único elem
-				pri = null;
+				pri = p = null;
 			} else if (p.obtenprev() == null) { // primer elem
 				p.obtensig().modprev(null);
 				pri = p.obtensig();
 				p.modsig(null);
+				p = pri;
+			} else if (p.obtensig() == null) { // último elem
+				p.obtenprev().modsig(null);
+				p.modprev(null);
+				p = null;
+			} else { // cualquier elemento
+				Nodo tmp = p.obtensig();
+				p.obtenprev().modsig(p.obtensig());
+				p.obtensig().modprev(p.obtenprev());
+				p.modprev(null);
+				p.modsig(null);
+				p = tmp;
 			}
 			++el;
 		}
 		return el;
 	}
-	// imprime lista
+	// modifica nodos con dato igual a v.data modificándolos para que su
+	// dato sea igual a n.data
+	public int mod(Nodo v, Nodo n) {
+		int m = 0;
+		if (v == null || n == null) return 0;
+		if ((m = remueve(v)) <= 0) return 0;
+		for (int i = 0; i < m; i++) inserta(new Nodo(n));
+		return m;
+	}
+
 	public void imprime() {
+		imprime(0);
+	}
+	// imprime lista
+	public void imprime(int debug) {
 		for (Nodo p = pri; p != null; p = p.obtensig()) {
-			System.out.println(p.dato);
+			if (debug > 0) {
+				System.err.println(p + " dato: " + p.dato + ": prev: " +
+				    p.obtenprev() + " sig: " + p.obtensig());
+			} else {
+				System.out.println(p.dato);
+			}
 		}
 	}
 }
